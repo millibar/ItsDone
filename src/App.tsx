6df06a21bf75b7375a57afb2e4a23694c67df635
millibar <millibarjp@gmail.com>
2023-01-09 18:10:29 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export const App = () => {
+  return <h1>It&#39;s Done</h1>;
+};
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      enabled: true,
+      reporter: ['text'],
+    },
+  },
+});
